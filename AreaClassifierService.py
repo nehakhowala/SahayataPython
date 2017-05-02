13ca2128ec7fefd991805fd44b9511c08833d241
Ankita Ravalji <ankitaravalji@gmail.com>
2017-05-01 20:17:27 -0700

    classifier function added

--- AreaClassifierService.py
+++ AreaClassifierService.py
@@ -12,3 +12,16 @@ crimeCountByLatLong = crimeData.round({
 
 def takeLatLongReturnCrimeNumbers(A,B):
     return int(crimeCountByLatLong.loc[(crimeCountByLatLong['X'] == A ) & (crimeCountByLatLong['Y'] == B )].crimeNumbers.values)
+
+def classifyArea(crimeCount):
+    if crimeCount>1000 :	
+        return "Zone 1 extremely unsafe area"
+    elif crimeCount>500 :
+        return "Zone 2 very high crime rate area"
+    elif crimeCount>100 :
+        return "Zone 3 moderately high crime rate area"
+    elif crimeCount>50 :
+        return "Zone 4 low crime rate area"
+    else:
+        return "Zone 5 safest area"
+    
